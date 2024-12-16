9821ca7db1f250d04f4ebed1ec0c2e6f2fc02753
Ionut <Ionut@DESKTOP-HLFSVSB>
2024-12-16 19:51:06 +0200

    Laborator4

--- Models/Author.cs
+++ Models/Author.cs
@@ -5,17 +5,16 @@ namespace Belean_Ioan_Daniel_Lab2.Models
     public class Author
     {
         public int ID { get; set; }
-
-        [Required]
-        [Display(Name = "First Name")]
         public string FirstName { get; set; }
-
-        [Required]
-        [Display(Name = "Last Name")]
         public string LastName { get; set; }
-
-        public string FullName => $"{FirstName} {LastName}";
-
-        public ICollection<Book>? Books { get; set; } 
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+        public ICollection<Book>? Books { get; set; }
     }
 }
--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Belean_Ioan_Daniel_Lab2.Data;
 using Belean_Ioan_Daniel_Lab2.Models;
+using Belean_Ioan_Daniel_Lab2.Models.ViewModels;
 
 namespace Belean_Ioan_Daniel_Lab2.Pages.Categories
 {
@@ -19,11 +20,35 @@ namespace Belean_Ioan_Daniel_Lab2.Pages.Categories
             _context = context;
         }
 
-        public IList<Category> Category { get;set; } = default!;
+        public CategoryIndexData CategoryData { get; set; } // ViewModel pentru categorie și cărți
+        public int CategoryID { get; set; } // ID-ul categoriei selectate
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? id)
         {
-            Category = await _context.Category.ToListAsync();
+            // Populăm datele pentru categorii și cărțile asociate
+            CategoryData = new CategoryIndexData
+            {
+                Categories = await _context.Category
+                    .Include(c => c.BookCategories)
+                    .ThenInclude(bc => bc.Book)
+                    .ThenInclude(b => b.Author)
+                    .OrderBy(c => c.CategoryName)
+                    .ToListAsync()
+            };
+
+            // Dacă există o categorie selectată, încărcăm cărțile acesteia
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                var selectedCategory = CategoryData.Categories
+                    .Where(c => c.ID == id.Value).SingleOrDefault();
+
+                if (selectedCategory != null)
+                {
+                    CategoryData.Books = selectedCategory.BookCategories
+                        .Select(bc => bc.Book);
+                }
+            }
         }
     }
 }
